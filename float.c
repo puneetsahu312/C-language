a0a61f2c3952d2690e2834fd8b9fd036a04c3c35
puneetsahu312 <puneetkumarsahu74@gmail.com>
2025-06-02 21:46:34 +0530

    Add files via upload

---
+++ float.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    float num1 = 5.1, num2 = 10.8, sum;
+    sum = num1 + num2;
+    printf(" sum = %.2df\n", sum);
+    return 0;
+}
---
+++ integer.c
@@ -0,0 +1,9 @@
+Q1) declear and print integer variable
+#include<stdio.h>
+int main()
+{
+    int num;         
+    num = 20;      
+    printf("The value of num is: %d\n", num);
+    return 0;
+}
