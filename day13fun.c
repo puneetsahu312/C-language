f79ec25ba356cf018fba2a792ed889460d03a5bc
puneetsahu312 <puneetkumarsahu74@gmail.com>
2025-06-26 18:29:25 +0530

    Add files via upload

---
+++ day13fun.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void add();
+void main()
+{
+    int sum;
+    printf("enter a and b values\n");
+    scanf("%d%d", &a,&b);
+    sum=a+b;
+    printf("\n The sum is %d", sum);
+}
